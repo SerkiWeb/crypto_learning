585c77fcdf30e6a6b5b1f89b2d730ff3c6b36bd5
Karim <kturki85@gmail.com>
2021-10-18 23:45:57 +0200

    add reward, distribution and refactoring

--- contracts/Paris.sol
+++ contracts/Paris.sol
@@ -7,7 +7,7 @@ contract Paris {
 
     address private _createur;
 
-    Outcome coteGagnante;
+    Outcome outcomeGagnant;
 
     uint256 totalReward;
 
@@ -25,20 +25,20 @@ contract Paris {
 
     mapping (address => Bet) markets;
 
-    constructor(uint256 coteEquipe1_, uint256 coteNul_, uint256 coteEquipe2_, uint duration) payable public {
-        require(msg.value > 0);
+    constructor(uint256 coteEquipe1_, uint256 coteNul_, uint256 coteEquipe2_, uint duration) public {
 
         _createur = msg.sender;
         _coteEquipe1 = coteEquipe1_;
         _coteNul = coteNul_;
         _coteEquipe2 = coteEquipe2_;
         endGameTime = block.timestamp + duration;
-        totalReward = msg.value;
+        totalReward = 0;
     }
 
     function addBet(uint256 outcome) payable public {
-        require(outcome == 0 || outcome == 1 || outcome == 2);
-        require(msg.value > 0);
+        require(outcome == 0 || outcome == 1 || outcome == 2, "wrong outcome");
+        require(msg.value > 0, "no ether was send");
+        require(block.timestamp <= endGameTime, "market is closed");
 
         if (outcome == 0) {
             markets[msg.sender] =  Bet(Outcome.EQUIPE1, _coteEquipe1, (_coteEquipe1 * msg.value));
@@ -49,16 +49,33 @@ contract Paris {
         } else {
             revert("Your outcome is not available");
         }
+
+        totalReward += msg.value;
     }
 
     function resultGame() public {
         require(msg.sender == _createur, "not authorized");
         require(block.timestamp > endGameTime, "event is not finished");
 
-        coteGagnante = Outcome.EQUIPE1;
+        outcomeGagnant = Outcome.EQUIPE1;
     }
 
     function getTotalReward() public view returns(uint256) {
         return totalReward;
+    }
+
+    function distributeReward() public payable {
+        require(block.timestamp > endGameTime, "event is not finished");
+        
+        if (markets[msg.sender].outcome == outcomeGagnant) {
+             msg.sender.transfer(totalReward);
+        }
+    }
+
+    function marketIsClosed() public returns(bool) {
+        bool marketIsClose =  (block.timestamp > endGameTime);
+
+        return marketIsClose;
+
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Paris = artifacts.require("Paris");
 
 module.exports = function (deployer) {
-    deployer.deploy(Paris, 1, 2, 3, 1000);
+    deployer.deploy(Paris, 1, 2, 3, 3600);
 };
