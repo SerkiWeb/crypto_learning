83d02bc4fe1dfdbfb447fbbbeca23717fc0b730d
Karim <kturki85@gmail.com>
2021-10-20 22:06:07 +0200

    set up http server + first call with web3.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-var http = require('http');
-var url = require('url');
-var Web3  = require('web3');
+var Web3      = require('web3');
+const express = require('express');
+const app     = express();
+const path    = require('path');
 var parisContract = require('../build/contracts/Paris.json');
 
-http.createServer(function (req, res) {
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/'))
+
+app.get('/' , (req , res)=>{
+    var reward = 0;
     let web3 = new Web3();
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
+    
     var myContract = new web3.eth.Contract( parisContract.abi, '0x39B16c46de5337c4dE37Ef9Daf17102A760104fa',  {
-        from: '0x1234567890123456789012345678901234567891', // default from address
-        gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
+        from: '0x61639B80171D8175760204636e3a56BdB931bdf6',
+        gasPrice: '20000000000'
     });
-    myContract.methods.getTotalReward().send({from : '0x61639B80171D8175760204636e3a56BdB931bdf6'})
-    .then(console.log)
-    .catch((err) =>
-        console.log(err)
-    );
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(req.url);
-    res.end();
-}).listen(8080);
+
+    myContract.methods.getTotalReward().call({from : '0x6781EC56a01c4331d39a221B1A0b24003B709C13'})
+    .then(function(receipt) {
+        reward = receipt;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    nameContract = parisContract.contractName;
+    res.render('market', {contractName : nameContract, reward: reward})
+})
+
+app.listen(8080, ()=>{
+    console.log('server running on port 8080');
+});
